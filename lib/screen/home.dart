5eb869493b2878b293999be489ae2a4b073d08eb
wudkung <phuriwatphanchalad@gmail.com>
2025-08-16 11:34:21 +0700

    Step 6 Images

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,81 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  // runApp(const MyApp());
-  // const app = MaterialApp(title: "หัวข้อ", home: Text("Hello World!"));
-  //runApp(app);
-
-  runApp(
-    MaterialApp(
-      title: "หัวข้อ",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My App"),
-          backgroundColor: Colors.blueAccent,
-          centerTitle: true,
-        ),
-        body: Home(),
-      ),
-    ),
-  );
-}
-
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //return Text("Hello Flutter! Step2");
-    //return Row(
     return Column(
-      // mainAxisAlignment: MainAxisAlignment.start,
-      // mainAxisAlignment: MainAxisAlignment.center,
-      // mainAxisAlignment: MainAxisAlignment.end,
-      // mainAxisAlignment: MainAxisAlignment.spaceAround,
-      // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-
       children: [
-        Container(
-          color: Colors.redAccent,
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: Text(
-            "Hello",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-        ),
-        Container(
-          color: Colors.redAccent,
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: Text(
-            "Hello",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-        ),
-        Container(
-          color: Colors.redAccent,
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: Text(
-            "Hello",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-        ),
+        Image.asset("assets/images/1.jpg", width: 200, height: 200),
+        const SizedBox(height: 20),
+        Image.network("https://picsum.photos/200/300", width: 200, height: 200),
       ],
     );
   }
