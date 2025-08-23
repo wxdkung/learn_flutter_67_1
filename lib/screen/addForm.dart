2ab3347ad8a40bafaf86d15ca2779e4497404153
wudkung <phuriwatphanchalad@gmail.com>
2025-08-23 11:55:23 +0700

    Step 15 : form state, Step 16 : submit button

--- lib/screen/addForm.dart
+++ lib/screen/addForm.dart
@@ -9,6 +9,11 @@ class AddForm extends StatefulWidget {
 }
 
 class _AddFormState extends State<AddForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _name = "";
+  int _age = 20;
+  Job _job = Job.engineer;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,38 +26,117 @@ class _AddFormState extends State<AddForm> {
         ),
         body: Padding(
           padding: EdgeInsets.all(15),
-          child: Column(
-            children: [
-              TextFormField(decoration: InputDecoration(labelText: "Name")),
-              TextFormField(
-                decoration: InputDecoration(labelText: "Age"),
-                keyboardType: TextInputType.number,
-              ),
-              DropdownButtonFormField<Job>(
-                decoration: InputDecoration(labelText: "Job"),
-                items: Job.values.map((key) {
-                  return DropdownMenuItem<Job>(
-                    value: key,
-                    child: Text(key.title),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  print("value = $value");
-                },
-              ),
-              SizedBox(height: 20),
-              FilledButton(
-                onPressed: () {},
-                style: FilledButton.styleFrom(
-                  backgroundColor: Colors.blueAccent,
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+          // child: Column(
+          //   children: [
+          //     TextFormField(decoration: InputDecoration(labelText: "Name")),
+          //     TextFormField(
+          //       decoration: InputDecoration(labelText: "Age"),
+          //       keyboardType: TextInputType.number,
+          //     ),
+          //     DropdownButtonFormField<Job>(
+          //       decoration: InputDecoration(labelText: "Job"),
+          //       items: Job.values.map((key) {
+          //         return DropdownMenuItem<Job>(
+          //           value: key,
+          //           child: Text(key.title),
+          //         );
+          //       }).toList(),
+          //       onChanged: (value) {
+          //         print("value = $value");
+          //       },
+          //     ),
+          //     SizedBox(height: 20),
+          //     FilledButton(
+          //       onPressed: () {},
+          //       style: FilledButton.styleFrom(
+          //         backgroundColor: Colors.blueAccent,
+          //         padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+          //       ),
+          //       child: Text(
+          //         "Submit",
+          //         style: TextStyle(color: Colors.white, fontSize: 20),
+          //       ),
+          //     ),
+          //   ],
+          // ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Name"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter name";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _name = value!;
+                  },
                 ),
-                child: Text(
-                  "Submit",
-                  style: TextStyle(color: Colors.white, fontSize: 20),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Age"),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter age";
+                    }
+                    if (int.tryParse(value) == null) {
+                      return "Please enter a valid number";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _age = int.parse(value!);
+                  },
                 ),
-              ),
-            ],
+
+                DropdownButtonFormField<Job>(
+                  decoration: InputDecoration(labelText: "Job"),
+                  value: _job,
+                  items: Job.values.map((key) {
+                    return DropdownMenuItem<Job>(
+                      value: key,
+                      child: Text(key.title),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _job = value!;
+                    });
+                  },
+                ),
+                SizedBox(height: 20),
+                FilledButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      Person newPerson = Person(
+                        name: _name,
+                        age: _age,
+                        job: _job,
+                      );
+                      setState(() {
+                        personList.add(newPerson);
+                      });
+                      ScaffoldMessenger.of(
+                        context,
+                      ).showSnackBar(SnackBar(content: Text("Person Added")));
+                      print("Name: $_name, Age: $_age, Job: ${_job.title}");
+                    }
+                  },
+                  style: FilledButton.styleFrom(
+                    backgroundColor: Colors.blueAccent,
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                  ),
+                  child: Text(
+                    "Submit",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
